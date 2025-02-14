8043af80f0f9f380651ba3122bc66e1a12e1f194
davide <db@celeria.ai>
2025-02-14 00:47:44 -0500

    fix: tsup config fixes

--- packages/asana/tsup.config.ts
+++ packages/asana/tsup.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: "esm",
   clean: true,
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  minify: true,
+  target: "esnext",
+  outDir: "dist",
 });
