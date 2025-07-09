95a895487b90d00ecaba419f3cbeca781f81f879
dom <dom@celeria.ai>
2025-07-09 19:37:29 +0000

    fix config values

--- packages/fetch/tsup.config.ts
+++ packages/fetch/tsup.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  format: "esm",
   clean: true,
-  banner: {
-    js: "#!/usr/bin/env node",
-  },
+  entry: ["src/index.ts"],
+  format: ["esm"],
+  minify: true,
+  target: "esnext",
+  outDir: "dist",
 });
